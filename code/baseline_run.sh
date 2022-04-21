4db4de1d5dd279f3126c6eae07132214e1b601df
t46 <takagi4646@gmail.com>
2022-04-21 18:36:40 +0900

    add scripts to run exp

---
+++ code/baseline_run.sh
@@ -0,0 +1,13 @@
+for seed in 666 42 1024;
+do
+python experiment.py --env hopper --dataset medium --model_type dt --seed $seed --pretrained_lm gpt2  --outdir "checkpoints/gpt2_medium_hopper_$seed" --dropout 0.2 --share_input_output_proj &
+done
+for seed in 666 42 1024;
+do
+python experiment.py --env walker2d --dataset medium --model_type dt --seed $seed  --pretrained_lm gpt2  --outdir "checkpoints/gpt2_medium_walker_$seed" --dropout 0.2 --share_input_output_proj &
+done
+for seed in 666 42 1024;
+do
+python experiment.py --env halfcheetah --dataset medium --model_type dt --seed $seed  --pretrained_lm gpt2  --outdir "checkpoints/chibiv2_kmeans__medium_positions_halfcheetah_$seed" --dropout 0.2 --share_input_output_proj &
+done
+wait
