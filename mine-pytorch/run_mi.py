3898f2122076018f1b81b895154ec59963bb0e99
t46 <takagi4646@gmail.com>
2022-10-04 22:43:30 +0900

    refactor run_mi.py

--- mine-pytorch/run_mi.py
+++ mine-pytorch/run_mi.py
@@ -1,62 +1,33 @@
-from mine.models.mine import Mine
-
+import argparse
 import numpy as np
-import torch.nn as nn
 from tqdm import tqdm
-import torch
+from util_mi_calculation import calc_mi, load_data_and_activation
+
 
 device = 'cuda'
 
-def calc_mi(X, Y):
-    x_dim = X.shape[1]
-    y_dim = Y.shape[1]
+def main(variant):
 
-    statistics_network = nn.Sequential(
-    nn.Linear(x_dim + y_dim, 400),
-    nn.ReLU(),
-    nn.Linear(400, 400),
-    nn.ReLU(),
-    nn.Linear(400, 400),
-    nn.ReLU(),
-    nn.Linear(400, 1)
-)
+    env_name = variant['env_name']
+    dataset_name = variant['dataset_name']
+    seed = variant['seed']
+    epoch = variant['epoch']
+    model_name = variant['model_name']
+    batch_size = variant['batch_size']
+    path_to_save_mi = variant['path_to_save_mi']
+    path_to_d4rl_data_sample = variant['path_to_data']
+    path_to_activation = variant['path_to_activation']
 
-    mine = Mine(
-        T = statistics_network,
-        loss = 'mine', #mine_biased, fdiv
-        method = 'concat'
-    ).to(device)
-
-    mi = mine.optimize(X, Y, iters = 1000, batch_size=50, opt=torch.optim.Adam(mine.parameters(), lr=1e-4))
-
-    del mine
-
-    return mi
-
-
-def main():
-
-    env_name = 'hopper'
-    dataset_name = 'medium'
-    seed = 666
-    epoch = 40
-    model_name = 'igpt'  # 'gpt2
-    batch_size = 100
-    path_to_save_mi = 'path_to_save_mi'
-    '''
-    Followings are the paths to data and activation saved by 
-    .../can-wikipdia-help-offline-rl/notebooks/section-52-mutual-information/save_activation.ipynb.
-    So refer to the notebook for the path.
-    '''
-    path_to_d4rl_data_sample = 'path_to_d4rl_data_sample'
-    path_to_activation = 'path_to_activation'
-
-    data = np.load(f'{path_to_d4rl_data_sample}/data_{env_name}_{dataset_name}_{seed}_{batch_size}.npy', allow_pickle=True).item()
-    activations = np.load(f'{path_to_activation}/activation_{epoch}_{model_name}_{env_name}_{dataset_name}_{seed}_{batch_size}.npy', allow_pickle=True).item()
-
-    rtg = data['rtg'][:, :-1].to(device)  # (batch_size, K, dim)
-    states = data['states'].to(device)  # (batch_size, K, dim)
-    actions = data['actions'].to(device)  # (batch_size, K, dim)
+    rtg, states, actions, activations = load_data_and_activation(path_to_d4rl_data_sample,
+                                                                 path_to_activation,
+                                                                 env_name,
+                                                                 dataset_name,
+                                                                 seed,
+                                                                 batch_size,
+                                                                 epoch,
+                                                                 model_name,
+                                                                 device
+                                                                )
 
     if model_name == 'igpt':
         keys = ['0.mlp.dropout', '12.mlp.dropout', '23.mlp.dropout']
@@ -86,12 +57,12 @@ def main():
                         rsa = actions
 
                     try:
-                        x_mi = calc_mi(rsa[:, step, :], activation[:, -2, :]).cpu().numpy()  # I(X; T)
+                        x_mi = calc_mi(rsa[:, step, :], activation[:, -2, :], device).cpu().numpy()  # I(X; T)
                     except:
                         x_mi = np.nan
                         print(f'{key}: x_mi is None')
                     try:
-                        y_mi = calc_mi(actions[:, -1, :], activation[:, (3 * step + i), :]).cpu().numpy()  # I(Y; T)
+                        y_mi = calc_mi(actions[:, -1, :], activation[:, (3 * step + i), :], device).cpu().numpy()  # I(Y; T)
                     except:
                         y_mi = np.nan
                         print(f'{key}: y_mi is None')
@@ -103,4 +74,15 @@ def main():
         np.save(f'{path_to_save_mi}/mi_xy_{key}_{epoch}_{model_name}_{env_name}_{dataset_name}_{seed}_{batch_size}.npy', mi_dict)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_save_mi", type=str)
+    parser.add_argument("--path_to_data", type=str)
+    parser.add_argument("--path_to_activation", type=str)
+    parser.add_argument("--env_name", type=str, default="hopper")
+    parser.add_argument("--dataset_name", type=str, default="medium")
+    parser.add_argument("--seed", type=int, default=666)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--epoch", type=int, default=40)
+    parser.add_argument("--model_name", type=str, default='gpt2')
+    args = parser.parse_args()
+    main(variant=vars(args))
