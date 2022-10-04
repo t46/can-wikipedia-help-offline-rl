4562ae3215c1872f83094a09053dca39319ed403
t46 <takagi4646@gmail.com>
2022-10-04 22:58:02 +0900

    refactor run_mi_data

--- mine-pytorch/run_mi_data.py
+++ mine-pytorch/run_mi_data.py
@@ -1,67 +1,41 @@
-from mine.models.mine import Mine
-
+import argparse
 import numpy as np
-import torch.nn as nn
 from tqdm import tqdm
-import os
-import torch
+from util_mi_calculation import calc_mi, load_data_and_activation
+
 
 device = 'cuda'
 
-def calc_mi(X, Y):
-    x_dim = X.shape[1]
-    y_dim = Y.shape[1]
-
-    statistics_network = nn.Sequential(
-    nn.Linear(x_dim + y_dim, 400),
-    nn.ReLU(),
-    nn.Linear(400, 400),
-    nn.ReLU(),
-    nn.Linear(400, 400),
-    nn.ReLU(),
-    nn.Linear(400, 1)
-)
-
-    mine = Mine(
-        T = statistics_network,
-        loss = 'mine', #mine_biased, fdiv
-        method = 'concat'
-    ).to(device)
-
-    mi = mine.optimize(X, Y, iters = 1000, batch_size=50, opt=torch.optim.Adam(mine.parameters(), lr=1e-4))
-
-    del mine
-
-    return mi
-
-
-def main():
+def main(variant):
     env_names = ['hopper', 'halfcheetah', 'walker2d']
-    dataset_name = 'medium'
-    seed = 666
-    batch_size = 100
-    path_to_save_mi = 'path_to_save_mi'
-    '''
-    The following is the path to data saved by 
-    .../can-wikipdia-help-offline-rl/notebooks/section-52-mutual-information/save_activation.ipynb.
-    So refer to the notebook for the path.
-    '''
-    path_to_d4rl_data_sample = 'path_to_d4rl_data_sample'
+    dataset_name = variant['dataset_name']
+    seed = variant['seed']
+    epoch = variant['epoch']
+    model_name = variant['model_name']
+    batch_size = variant['batch_size']
+    path_to_save_mi = variant['path_to_save_mi']
+    path_to_d4rl_data_sample = variant['path_to_data']
+    path_to_activation = variant['path_to_activation']
 
     for env_name in env_names:
 
-        data = np.load(f'{path_to_d4rl_data_sample}/data_{env_name}_{dataset_name}_{seed}_{batch_size}.npy', allow_pickle=True).item()
-
-        rtg = data['rtg'][:, :-1].to(device)  # (batch_size, K, dim)
-        states = data['states'].to(device)  # (batch_size, K, dim)
-        actions = data['actions'].to(device)  # (batch_size, K, dim)
+        rtg, states, actions, _ = load_data_and_activation(path_to_d4rl_data_sample,
+                                                           path_to_activation,
+                                                           env_name,
+                                                           dataset_name,
+                                                           seed,
+                                                           batch_size,
+                                                           epoch,
+                                                           model_name,
+                                                           device
+                                                           )
 
         mi_state_action_list = []
         mi_rtg_action_list = []
         for step in tqdm(range(states.shape[1])):
             try:
-                mi_state_action = calc_mi(states[:, step, :], actions[:, step, :])
-                mi_rtg_action = calc_mi(rtg[:, step, :], actions[:, step, :])
+                mi_state_action = calc_mi(states[:, step, :], actions[:, step, :], device)
+                mi_rtg_action = calc_mi(rtg[:, step, :], actions[:, step, :], device)
             except:
                 mi_state_action = None
                 mi_rtg_action = None
@@ -73,4 +47,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_save_mi", type=str)
+    parser.add_argument("--path_to_data", type=str)
+    parser.add_argument("--path_to_activation", type=str)
+    parser.add_argument("--dataset_name", type=str, default="medium")
+    parser.add_argument("--seed", type=int, default=666)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--epoch", type=int, default=40)
+    parser.add_argument("--model_name", type=str, default='gpt2')
+    args = parser.parse_args()
+    main(variant=vars(args))
